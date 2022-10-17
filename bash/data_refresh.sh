ec0de0401f0966087d30d4d4ca355c51469ef78c
AhmedAlmustfa <98463028+AhmedAlmustfa@users.noreply.github.com>
2022-10-17 09:45:42 +0200

    Update data_refresh.sh

--- bash/data_refresh.sh
+++ bash/data_refresh.sh
@@ -5,8 +5,8 @@ echo "Rendering the dashboard..."
 Rscript -e "rmarkdown::render_site()"
 
 if [[ "$(git status --porcelain)" != "" ]]; then
-    git config --global user.name 'RamiKrispin'
-    git config --global user.email 'ramkrisp@umich.edu'
+    git config --global user.name 'AhmedAlmustfa'
+    git config --global user.email 'Ahmedkhatir22@gmail.com'
     git add *
     git commit -m "Auto update dashboard"
     git push

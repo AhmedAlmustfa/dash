164740d348e76f7e859a8bae64f3d5efbff0c0d3
AhmedAlmustfa <98463028+AhmedAlmustfa@users.noreply.github.com>
2022-10-19 14:03:13 +0200

    Update build_docker.sh

--- docker/packages/build_docker.sh
+++ docker/packages/build_docker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker build . -t docker.io/rkrispin/covid19italy_dash:dev
+docker build . -t rkrispin/flex_dash_env:dev.0.0.0.9000
 
 if [[ $? = 0 ]] ; then
 echo "Pushing docker..."
-docker push docker.io/rkrispin/covid19italy_dash:dev
+docker push rkrispin/flex_dash_env:dev.0.0.0.9000
 else
 echo "Docker build failed"
 fi
